c6bb59b2b01f6d553b7cd0ed7b1f2c9d226a6493
Ann <a.m.hnatyshyn@gmail.com>
2024-05-25 14:36:48 -0300

    new file, due to mistake on github

--- fetcher.js
+++ fetcher.js
@@ -2,33 +2,22 @@ const needle = require("needle");
 needle.get("http://www.example.edu/", (error, response, body) => {
   console.log("error:", error); // Print the error if one occurred
   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML
+  // console.log("body:", body); // Print the HTML
 });
 
 const fs = require("fs").promises;
 
 const fetcher = async() => {
   try {
-    const filePath = "/Users/annhnatyshyn/lighthouse/page-fetcher";
-    const fileDescriptor = await fs.open(filePath, "r");
-
-    const content = "Some content!";
-    fileDescriptor;
-    await fs.writeFile("/Users/annhnatyshyn/lighthouse/page-fetcher", content);
-
+    const filePath = "/Users/annhnatyshyn/lighthouse/page-fetcher/index.js";
+    const URL = "http://www.example.edu";
+    // Fetch the content from the URL
+    const response = await needle.get(URL);
+    // Write the fetched data to the file
+    await fs.appendFile(filePath, response);
     console.log("File written successfully");
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
   }
 };
 fetcher();
-
-// const fletcher = async function() {
-//   try {
-//     const content = 'Some content!';
-//     await fs.appendFile(filePath, content);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-// fletcher();
